9675f1c4b0622565a42212d71ee2f0fea376fbe7
Diego-Avanzatti <avanzattidiego@gmail.com>
2025-08-09 16:32:00 -0300

    mas arreglos

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -5,4 +5,4 @@ def index(request):
     return render(request, 'index.html')
 
 def pagina_404(request, exception):
-    return HttpResponseNotFound('<h1>Pagina no encontrada</h1>')
+    return render(request,'genericos/404.html')
